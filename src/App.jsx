1c08f4fcee0371b4b4ddca814780b5326e957d6c
Shahid khan <professionalshahid0@gmail.com>
2024-03-19 21:39:05 +0530

    Started Creating Portfolio

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="bg-[url('/Images/background.jpg')] bg-opacity-1 bg-fixed bg-cover bg-center">
+      <div className='text-white w-full h-screen'>
+        hello
+  </div>
+      <div className='text-white w-full h-screen'>
+        hello
+  </div>
+    </div>
+  )
+}
+
+export default App
