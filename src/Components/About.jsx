a2aff0c0b18e0fc6fe696fc9262ef7e8603a7da4
Shahid khan <professionalshahid0@gmail.com>
2024-03-21 00:13:32 +0530

    Reduced redundancy

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,18 +1,33 @@
 import React from "react";
 import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 
+// Reusable component for social media icons
+const SocialMediaIcon = ({ icon }) => (
+  <span className="text-4xl text-blue-500 mt-5 bg-white/[0.1] p-5 rounded-full mr-3">
+    <a href={icon.link}>
+      <i className={`fa-brands fa-${icon.name}`}></i>
+    </a>
+  </span>
+);
+
 function About() {
   return (
-    <div className=" mt-20 flex ">
-      <div className=" overflow-hidden bg-cover bg-center mt-36">
-        <img src="/Images/profile-pic.png" className=""></img>
+    <div className="mt-20 flex">
+      {/* Image */}
+      <div className="overflow-hidden bg-cover bg-center mt-36">
+        <img src="/Images/profile-pic.png" alt="Profile" />
       </div>
+
+      {/* Text Content */}
       <div className="ml-20">
+        {/* Heading */}
         <h1 className="absolute text-white text-[180px] font-extrabold tracking-widest opacity-[0.2]">
           ABOUT
         </h1>
-        <div className="relative top-32 flex justify-center items flex-col">
-          <h1 className="text-5xl text-white mb-20 ">Hi! I'm Shahid khan</h1>
+
+        {/* Main Content */}
+        <div className="relative top-32 flex flex-col">
+          <h1 className="text-5xl text-white mb-20">Hi! I'm Shahid khan</h1>
           <div className="text-3xl text-subheading w-[700px] tracking-widest text-balance">
             An engineering student passionate about front-end development with
             React.js. I specialize in creating user-friendly web applications
@@ -20,27 +35,17 @@ function About() {
             something amazing together!
           </div>
 
-          <span className="text-3xl text-blue-500 mt-5">
-            <a>Checkout My Resume</a>
-          </span>
+          {/* Social Media Icons */}
           <div className="flex">
-            <span className="text-4xl text-blue-500 mt-5 bg-white/[0.1] p-5 rounded-full mr-3">
-              <a>
-                <i class="fa-brands fa-linkedin"></i>
-              </a>
-            </span>
-            <span className="text-4xl text-blue-500 mt-5 bg-white/[0.1] p-5 rounded-full mr-3">
-              <a>
-              <i class="fa-brands fa-github"></i>
-              </a>
-            </span>
-            <span className="text-4xl text-blue-500 mt-5 bg-white/[0.1] p-5 rounded-full ">
-              <a>
-              <i class="fa-brands fa-instagram"></i>
-              </a>
-            </span>
-
+            <SocialMediaIcon icon={{ name: "linkedin", link: "https://www.linkedin.com/in/shahid-khan-7a97051b8/" }} />
+            <SocialMediaIcon icon={{ name: "github", link: "https://github.com/Shahid0301" }} />
+            <SocialMediaIcon icon={{ name: "instagram", link: "https://www.instagram.com/shahid_0301/" }} />
           </div>
+          
+          {/* Link to Resume */}
+          <span className="text-3xl text-blue-500 mt-5">
+            <a href="link_to_your_resume">Checkout My Resume</a>
+          </span>
         </div>
       </div>
     </div>
