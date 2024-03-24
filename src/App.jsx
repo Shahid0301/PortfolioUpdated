5855543b9e88ed6b17edd87303cce1bddc701493
Shahid khan <professionalshahid0@gmail.com>
2024-03-24 23:56:21 +0530

    Started Skills section

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import Introduction from "./Components/Introduction";
 import About from "./Components/About";
 import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+import Skills from "./Components/Skills";
 
 function App() {
   return (
     <div className="bg-[url('/Images/background.jpg')] bg-opacity-1 bg-fixed bg-cover bg-center font-Poppins ">
       <Introduction />
       <About/>
-   
+      <Skills/>
 
     </div>
   );
---
+++ src/Components/Skills.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Skills() {
+  return (
+      <div className='w-full flex  h-screen mt-36 justify-around'>
+          <h1 className="absolute  text-white text-[180px] font-extrabold tracking-widest opacity-[0.2]">
+          SKILLS
+        </h1>
+          <div className='w-1/3 border-2 border-white'>
+              
+          </div>
+          <div className='w-1/3 border-2 border-white'>
+              
+          </div>
+    </div>
+  )
+}
+
+export default Skills
